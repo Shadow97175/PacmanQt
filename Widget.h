758c528fb93326d441d8b5dfc88f70504f892e7d
Shadow97175 <Shadow_97@mail.ru>
2015-09-02 08:43:35 +0300

    Update Widget.h

--- Widget.h
+++ Widget.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QPixmap>
+#include <vector>
+#include <QString>
 
 namespace Ui {
 class Widget;
@@ -20,12 +22,52 @@ const int emptyField = 0,
     wall = 3,
     backgroundCount = 4;
 
+const int red = 0,
+    blue = 1,
+    orange = 2,
+    pink = 3,
+    ghostColorCount = 4;
+
+const int ghostEatableStart = 0,
+    ghostEatableEnd = 1,
+    ghostModeCount = 2;
+
+const int firstPicture = 0,
+    secondPicture = 1,
+    pictureCount = 2;
+
+const int gameStarted = 0,
+    gameStopped = 1,
+    gameStateCount = 2;
+
 const int levelWidth = 28,
     levelHeight = 31;
 
-map.resize(levelHeight, vector<Width>(levelWidth));
+const int topPanelHeight = 50;
 
-vector <vector <Width>> map =
+const QString directionNames[directionCount] =
+{
+    "Left",
+    "Up",
+    "Right",
+    "Down"
+};
+
+const QString colorNames[ghostColorCount] =
+{
+    "Red",
+    "Blue",
+    "Orange",
+    "Pink"
+};
+
+const QString modeNames[ghostModeCount] =
+{
+    "Start",
+    "End"
+};
+
+const int initialMap[levelHeight][levelWidth] =
 {
     {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
     {3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3},
@@ -41,7 +83,7 @@ vector <vector <Width>> map =
     {0, 0, 0, 0, 0, 3, 1, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1, 3, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 3, 1, 3, 3, 0, 3, 3, 3, 0, 0, 3, 3, 3, 0, 3, 3, 1, 3, 0, 0, 0, 0, 0},
     {3, 3, 3, 3, 3, 3, 1, 3, 3, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 3, 3, 1, 3, 3, 3, 3, 3, 3},
-    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 3, 3, 3, 3, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
     {3, 3, 3, 3, 3, 3, 1, 3, 3, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 3, 3, 1, 3, 3, 3, 3, 3, 3},
     {0, 0, 0, 0, 0, 3, 1, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 3, 1, 3, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 3, 1, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1, 3, 0, 0, 0, 0, 0},
@@ -58,7 +100,7 @@ vector <vector <Width>> map =
     {3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3},
     {3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3},
     {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
-}
+};
 
 class Widget : public QWidget
 {
@@ -71,43 +113,47 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
-    QPixmap pacman[directionCount];
-    QPixmap pacmanClosed[directionCount];
+    QPixmap pacman[pictureCount][directionCount];
     QPixmap background[backgroundCount];
-    QPixmap redGhost[directionCount];
-    QPixmap redGhostClosed[directionCount];
-    QPixmap blueGhost[directionCount];
-    QPixmap blueGhostClosed[directionCount];
-    QPixmap pinkGhost[directionCount];
-    QPixmap pinkGhostClosed[directionCount];
-    QPixmap orangeGhost[directionCount];
-    QPixmap orangeGhostClosed[directionCount];
+
+    QPixmap ghost[ghostColorCount][pictureCount][directionCount];
+    QPixmap deathGhost[ghostModeCount][pictureCount];
 
     int pacmanDirection;
     int pacmanX;
     int pacmanY;
 
-    int redGhostDirection;
-    int redGhostX;
-    int redGhostY;
+    int ghostDirection[ghostColorCount];
+    int ghostX[ghostColorCount];
+    int ghostY[ghostColorCount];
 
-    int blueGhostDirection;
-    int blueGhostX;
-    int blueGhostY;
-
-    int pinkGhostDirection;
-    int pinkGhostX;
-    int pinkGhostY;
-
-    int orangeGhostDirection;
-    int orangeGhostX;
-    int orangeGhostY;
+    /*int deathGhostDirection[modeNames];
+    int deathGhostX[modeNames];
+    int deathGhostY[modeNames];*/
 
     int cellSize;
     bool closed;
 
+    int score;
+    int eatAnimalsTimeLeft;
+    int lifeCount;
+    int gameState;
+    bool keyIsHold;
+    int lastKey;
+
+    QTimer *timer;
+
+    std::vector<std::vector<int> > map;
+
     void paintEvent(QPaintEvent *);
     void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent *);
+
+    int getRightDirectionToPacman(int color);
+    void initGame();
+    void initLevel();
+    void changeDirection();
+    void checkGameState();
 
 public slots:
     void onTimer();
