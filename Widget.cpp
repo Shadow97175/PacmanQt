5f7d3da6dbcf9b2e9dea763735fa61367d337c03
Shadow97175 <Shadow_97@mail.ru>
2015-09-02 08:47:23 +0300

    Update Widget.cpp

--- Widget.cpp
+++ Widget.cpp
@@ -3,90 +3,60 @@
 #include <QTimer>
 #include <QPainter>
 #include <QKeyEvent>
+#include <QMessageBox>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget),
-    pacmanDirection(left),
-    pacmanX(13),
-    pacmanY(23),
-
-    redGhostDirection(left),
-    redGhostX(13),
-    redGhostY(11),
-
-    blueGhostDirection(up),
-    blueGhostX(11),
-    blueGhostY(14),
-
-    pinkGhostDirection(down),
-    pinkGhostX(13),
-    pinkGhostY(14),
-
-    orangeGhostDirection(up),
-    orangeGhostX(15),
-    orangeGhostY(14),
 
     cellSize(20),
-    closed(false)
-
+    closed(false),
+    keyIsHold(false),
+    lastKey(-1)
 {
-    pacman[right].load(":/Pictures/Pacman/PacmanRight.png");
-    pacman[left].load(":/Pictures/Pacman/PacmanLeft.png");
-    pacman[up].load(":/Pictures/Pacman/PacmanUp.png");
-    pacman[down].load(":/Pictures/Pacman/PacmanDown.png");
-
-    pacmanClosed[right].load(":/Pictures/Pacman/PacmanRightClosed.png");
-    pacmanClosed[left].load(":/Pictures/Pacman/PacmanLeftClosed.png");
-    pacmanClosed[up].load(":/Pictures/Pacman/PacmanUpClosed.png");
-    pacmanClosed[down].load(":/Pictures/Pacman/PacmanDownClosed.png");
+    for (int picture = 0; picture < pictureCount; picture++)
+    {
+        for (int direction = 0; direction < directionCount; direction++)
+        {
+            pacman[picture][direction].load(QString(":/Pictures/Pacman/Pacman%1%2.png")
+                                            .arg(directionNames[direction])
+                                            .arg(picture + 1/*для нумерации с 1, а не с 0*/));
+        }
+    }
 
     background[emptyField].load(":/Pictures/Map/EmptyField.png");
     background[food].load(":/Pictures/Map/Food.png");
     background[eatAnimals].load(":/Pictures/Map/EatAnimals.png");
     background[wall].load(":/Pictures/Map/Wall.png");
 
-    redGhost[right].load(":/Pictures/Ghosts/RedGhost/RedGhostRight.png");
-    redGhost[left].load(":/Pictures/Ghosts/RedGhost/RedGhostLeft.png");
-    redGhost[up].load(":/Pictures/Ghosts/RedGhost/RedGhostUp.png");
-    redGhost[down].load(":/Pictures/Ghosts/RedGhost/RedGhostDown.png");
+    for (int mode = 0; mode < ghostModeCount; mode++)
+    {
+        for (int picture = 0; picture < pictureCount; picture++)
+        {
+            deathGhost[mode][picture].load(QString(":/Pictures/Ghosts/DeathMode/DeathMode%1%2.png")
+                                                      .arg(modeNames[mode])
+                                                      .arg(picture + 1/*для нумерации с 1, а не с 0*/));
+        }
+    }
 
-    redGhostClosed[right].load(":/Pictures/Ghosts/RedGhost/RedGhostRightClosed.png");
-    redGhostClosed[left].load(":/Pictures/Ghosts/RedGhost/RedGhostLeftClosed.png");
-    redGhostClosed[up].load(":/Pictures/Ghosts/RedGhost/RedGhostUpClosed.png");
-    redGhostClosed[down].load(":/Pictures/Ghosts/RedGhost/RedGhostDownClosed.png");
+    for (int color = 0; color < ghostColorCount; color++)
+    {
+        for (int direction = 0; direction < directionCount; direction++)
+        {
+            for (int picture = 0; picture < pictureCount; picture++)
+            {
+                ghost[color][picture][direction].load(QString(":/Pictures/Ghosts/%1Ghost/%1Ghost%2%3.png")
+                                               .arg(colorNames[color])
+                                               .arg(directionNames[direction])
+                                               .arg(picture + 1/*для нумерации с 1, а не с 0*/));
+            }
+        }
+    }
 
-    blueGhost[right].load(":/Pictures/Ghosts/BlueGhost/BlueGhostRight.png");
-    blueGhost[left].load(":/Pictures/Ghosts/BlueGhost/BlueGhostLeft.png");
-    blueGhost[up].load(":/Pictures/Ghosts/BlueGhost/BlueGhostUp.png");
-    blueGhost[down].load(":/Pictures/Ghosts/BlueGhost/BlueGhostDown.png");
+    initGame();
+    initLevel();
 
-    blueGhostClosed[right].load(":/Pictures/Ghosts/BlueGhost/BlueGhostRightClosed.png");
-    blueGhostClosed[left].load(":/Pictures/Ghosts/BlueGhost/BlueGhostLeftClosed.png");
-    blueGhostClosed[up].load(":/Pictures/Ghosts/BlueGhost/BlueGhostUpClosed.png");
-    blueGhostClosed[down].load(":/Pictures/Ghosts/BlueGhost/BlueGhostDownClosed.png");
-
-    pinkGhost[right].load(":/Pictures/Ghosts/PinkGhost/PinkGhostRight.png");
-    pinkGhost[left].load(":/Pictures/Ghosts/PinkGhost/PinkGhostLeft.png");
-    pinkGhost[up].load(":/Pictures/Ghosts/PinkGhost/PinkGhostUp.png");
-    pinkGhost[down].load(":/Pictures/Ghosts/PinkGhost/PinkGhostDown.png");
-
-    pinkGhostClosed[right].load(":/Pictures/Ghosts/PinkGhost/PinkGhostRightClosed.png");
-    pinkGhostClosed[left].load(":/Pictures/Ghosts/PinkGhost/PinkGhostLeftClosed.png");
-    pinkGhostClosed[up].load(":/Pictures/Ghosts/PinkGhost/PinkGhostUpClosed.png");
-    pinkGhostClosed[down].load(":/Pictures/Ghosts/PinkGhost/PinkGhostDownClosed.png");
-
-    orangeGhost[right].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostRight.png");
-    orangeGhost[left].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostLeft.png");
-    orangeGhost[up].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostUp.png");
-    orangeGhost[down].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostDown.png");
-
-    orangeGhostClosed[right].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostRightClosed.png");
-    orangeGhostClosed[left].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostLeftClosed.png");
-    orangeGhostClosed[up].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostUpClosed.png");
-    orangeGhostClosed[down].load(":/Pictures/Ghosts/OrangeGhost/OrangeGhostDownClosed.png");
-
-    QTimer *timer = new QTimer(this);
+    timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(onTimer()));
     timer->start(125);
 }
@@ -100,15 +70,48 @@ void Widget::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
 
+    if (keyIsHold)
+    {
+        changeDirection();
+    }
+
+    if (gameState == gameStopped)
+    {
+        return;
+    }
+
+    if (eatAnimalsTimeLeft > 0)
+    {
+        painter.drawText(0, 0, cellSize * levelWidth, topPanelHeight, Qt::AlignCenter, QString("Lifes: %1 Score: %2 Eat time left: %3s").arg(lifeCount).arg(score).arg(eatAnimalsTimeLeft / 8));
+    }
+    else
+    {
+        painter.drawText(0, 0, cellSize * levelWidth, topPanelHeight, Qt::AlignCenter, QString("Lifes: %1 Score: %2").arg(lifeCount).arg(score));
+    }
+
     switch(pacmanDirection)
     {
     case left:
-        if (pacmanX>0 && map[pacmanY][pacmanX-1]!=wall)
-            pacmanX--;
+        if (pacmanX == 0)
+        {
+            pacmanX = levelWidth - 1;
+        }
+        else
+        {
+            if (map[pacmanY][pacmanX-1] != wall)
+                pacmanX--;
+        }
         break;
     case right:
-        if (pacmanX<levelWidth - 1 && map[pacmanY][pacmanX+1]!=wall)
-            pacmanX++;
+        if (pacmanX == levelWidth - 1)
+        {
+            pacmanX = 0;
+        }
+        else
+        {
+            if (pacmanX<levelWidth - 1 && map[pacmanY][pacmanX+1]!=wall)
+                pacmanX++;
+        }
         break;
     case up:
         if (pacmanY>0 && map[pacmanY-1][pacmanX]!=wall)
@@ -120,48 +123,250 @@ void Widget::paintEvent(QPaintEvent *)
         break;
     }
 
+    checkGameState();
+
+    //Ghosts
+    for (int color = 0; color < ghostColorCount; color++)
+    {
+        ghostDirection[color] = getRightDirectionToPacman(color);
+        switch(ghostDirection[color])
+        {
+        case left:
+            if (ghostX[color] == 0)
+            {
+                ghostX[color] = levelWidth - 1;
+            }
+            else
+            {
+                if (map[ghostY[color]][ghostX[color]-1] != wall)
+                    ghostX[color]--;
+            }
+            break;
+        case right:
+            if (ghostX[color] == levelWidth - 1)
+            {
+                ghostX[color] = 0;
+            }
+            else
+            {
+                if (ghostX[color]<levelWidth - 1 && map[ghostY[color]][ghostX[color]+1]!=wall)
+                    ghostX[color]++;
+            }
+            break;
+        case up:
+            if (ghostY[color]>0 && map[ghostY[color]-1][ghostX[color]]!=wall)
+                ghostY[color]--;
+            break;
+        case down:
+            if (ghostY[color]<levelHeight - 1 && map[ghostY[color]+1][ghostX[color]]!=wall)
+                ghostY[color]++;
+            break;
+        }
+    }
+
+    checkGameState();
+
+    if (map[pacmanY][pacmanX] == food)
+    {
+        score++;
+        map[pacmanY][pacmanX] = emptyField;
+    }
+
+    if (map[pacmanY][pacmanX] == eatAnimals)
+    {
+        eatAnimalsTimeLeft = 10 * 8;
+        map[pacmanY][pacmanX] = emptyField;
+    }
+
+    int foodLeftCount = 0;
     for (int y = 0; y < levelHeight; y++)
         for (int x = 0; x < levelWidth; x++)
         {
-            painter.drawPixmap(x * cellSize, y * cellSize, cellSize, cellSize, background[map[y][x]]);
+            painter.drawPixmap(x * cellSize, topPanelHeight + y * cellSize, cellSize, cellSize, background[map[y][x]]);
+            if (map[y][x] == food)
+            {
+                foodLeftCount++;
+            }
         }
 
-    if (closed)
+    if (foodLeftCount == 0)
     {
-        painter.drawPixmap(pacmanX * cellSize, pacmanY * cellSize, cellSize, cellSize, pacmanClosed[pacmanDirection]);
-        painter.drawPixmap(redGhostX * cellSize, redGhostY * cellSize, cellSize, cellSize, redGhostClosed[redGhostDirection]);
-        painter.drawPixmap(blueGhostX * cellSize, blueGhostY * cellSize, cellSize, cellSize, blueGhostClosed[blueGhostDirection]);
-        painter.drawPixmap(pinkGhostX * cellSize, pinkGhostY * cellSize, cellSize, cellSize, pinkGhostClosed[pinkGhostDirection]);
-        painter.drawPixmap(orangeGhostX * cellSize, orangeGhostY * cellSize, cellSize, cellSize, orangeGhostClosed[orangeGhostDirection]);
+        gameState = gameStopped;
+        QMessageBox::information(this, "Congratulation", "You win");
+        initGame();
+        initLevel();
+    }
+
+    painter.drawPixmap(pacmanX * cellSize, topPanelHeight + pacmanY * cellSize, cellSize, cellSize, pacman[closed ? secondPicture : firstPicture][pacmanDirection]);
+    if (eatAnimalsTimeLeft == 0)
+    {
+        for (int color = 0; color < ghostColorCount; color++)
+        {
+            painter.drawPixmap(ghostX[color] * cellSize,
+                               topPanelHeight + ghostY[color] * cellSize,
+                               cellSize, cellSize,
+                               ghost[color][closed ? secondPicture : firstPicture][ghostDirection[color]]);
+        }
     }
     else
     {
-        painter.drawPixmap(pacmanX * cellSize, pacmanY * cellSize, cellSize, cellSize, pacman[pacmanDirection]);
-        painter.drawPixmap(redGhostX * cellSize, redGhostY * cellSize, cellSize, cellSize, redGhost[redGhostDirection]);
-        painter.drawPixmap(blueGhostX * cellSize, blueGhostY * cellSize, cellSize, cellSize, blueGhost[blueGhostDirection]);
-        painter.drawPixmap(pinkGhostX * cellSize, pinkGhostY * cellSize, cellSize, cellSize, pinkGhost[pinkGhostDirection]);
-        painter.drawPixmap(orangeGhostX * cellSize, orangeGhostY * cellSize, cellSize, cellSize, orangeGhost[orangeGhostDirection]);
+        for (int color = 0; color < ghostColorCount; color++)
+        {
+            painter.drawPixmap(ghostX[color] * cellSize,
+                               topPanelHeight + ghostY[color] * cellSize,
+                               cellSize, cellSize,
+                               deathGhost[eatAnimalsTimeLeft > 3 * 8 ? ghostEatableStart : ghostEatableEnd][closed ? secondPicture : firstPicture]);
+        }
     }
 
     closed = !closed;
+    if (eatAnimalsTimeLeft > 0)
+        eatAnimalsTimeLeft--;
 }
 
 void Widget::keyPressEvent(QKeyEvent *event)
 {
-    switch (event->key())
+    lastKey = event->key();
+    changeDirection();
+    keyIsHold = true;
+}
+
+void Widget::keyReleaseEvent(QKeyEvent*)
+{
+    keyIsHold = false;
+}
+
+int Widget::getRightDirectionToPacman(int color)
+{
+    int x = ghostX[color];
+    int y = ghostY[color];
+    int previousDirection = ghostDirection[color];
+    switch (previousDirection)
+    {
+    case left:
+    case right:
+        if (map[y - 1][x] == wall && map[y + 1][x] == wall)
+            return previousDirection;
+    case up:
+    case down:
+        if (map[y][x - 1] == wall && map[y][x + 1] == wall)
+            return previousDirection;
+    }
+    int direction = rand() * directionCount / (RAND_MAX + 1);
+    while ((direction + previousDirection) % 2 == 0)
+    {
+        direction = rand() * directionCount / (RAND_MAX + 1);
+        switch (direction)
+        {
+        case left:
+            if (x == 0 || map[y][x - 1] == wall)
+                direction = previousDirection;
+            break;
+        case up:
+            if (y == 0 || map[y - 1][x] == wall)
+                direction = previousDirection;
+            break;
+        case right:
+            if (x == levelWidth - 1 || map[y][x + 1] == wall)
+                direction = previousDirection;
+            break;
+        case down:
+            if (y == levelHeight - 1 || map[y + 1][x] == wall)
+                direction = previousDirection;
+            break;
+        }
+    }
+    return direction;
+}
+
+void Widget::initGame()
+{
+    pacmanDirection = left;
+    pacmanX = 13;
+    pacmanY = 23;
+
+    ghostDirection[red] = left;
+    ghostX[red] = 13;
+    ghostY[red] = 11;
+
+    ghostDirection[blue] = up;
+    ghostX[blue] = 11;
+    ghostY[blue] = 15;
+
+    ghostDirection[pink] = down;
+    ghostX[pink] = 13;
+    ghostY[pink] = 15;
+
+    ghostDirection[orange] = up;
+    ghostX[orange] = 15;
+    ghostY[orange] = 15;
+
+    gameState = gameStarted;
+}
+
+void Widget::initLevel()
+{
+    score = 0;
+    lifeCount = 3;
+    eatAnimalsTimeLeft = 0;
+    map.clear();
+    for (int y = 0; y < levelHeight; y++)
+    {
+        map.push_back(std::vector<int>());
+        for (int x = 0; x < levelWidth; x++)
+        {
+            map[y].push_back(initialMap[y][x]);
+        }
+    }
+}
+
+void Widget::changeDirection()
+{
+    switch (lastKey)
     {
     case Qt::Key_Left:
-        pacmanDirection = left;
+        if (pacmanX > 0 && map[pacmanY][pacmanX - 1] != wall)
+            pacmanDirection = left;
         break;
     case Qt::Key_Up:
-        pacmanDirection = up;
+        if (pacmanY > 0 && map[pacmanY - 1][pacmanX] != wall)
+            pacmanDirection = up;
         break;
     case Qt::Key_Right:
-        pacmanDirection = right;
+        if (pacmanX < levelWidth && map[pacmanY][pacmanX + 1] != wall)
+            pacmanDirection = right;
         break;
     case Qt::Key_Down:
-        pacmanDirection = down;
+        if (pacmanY < levelHeight && map[pacmanY + 1][pacmanX] != wall)
+            pacmanDirection = down;
         break;
+    }
+}
+
+void Widget::checkGameState()
+{
+    for (int color = 0; color < ghostColorCount; color++)
+    {
+        if (pacmanY == ghostY[color] && pacmanX == ghostX[color])
+        {
+            if (eatAnimalsTimeLeft > 0)
+            {
+                ghostX[color] = 14;
+                ghostY[color] = 14;
+                score += 10;
+            }
+            else
+            {
+                //gameState = gameStopped;
+                lifeCount--;
+                initGame();
+                if (lifeCount == 0)
+                {
+                    QMessageBox::critical(this, "Game over", "You failed");
+                    initLevel();
+                }
+            }
+        }
     }
 }
 
@@ -169,17 +374,3 @@ void Widget::onTimer()
 {
     update();
 }
-
-/*bool check_path(Point start, Point finish)
-    {
-        while (!(start == finish))
-        {
-            if (get_block(start) == block::wall)        проверка пути
-                return false;
-
-            start.x += sign(finish.x - start.x);
-            start.y += sign(finish.y - start.y);
-        }
-
-        return true;
-    }*/
